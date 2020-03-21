a48b54317bce5c0ebb6ac6f3ecc92e533572ae68
Ingrid Amaral <guid_95@hotmail.com>
2020-03-21 15:32:37 +0100

    content component with style

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Trial from './homepage/Trial';
 import './App.css';
+import Content from './homepage/Content/Content';
 
 class App extends React.Component {
     render() {
         return (
             <div className='app_container'>
                 <Trial />
+                <Content />
             </div>
         )
     }
---
+++ src/components/homepage/Content/Content.js
@@ -0,0 +1,62 @@
+import React from "react";
+import "./Content.css";
+
+class Content extends React.Component {
+  render() {
+    const content = [
+      {
+        title: "Enjoy on your TV.",
+        text:
+          "Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.",
+        imgUrl:
+          "https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/tv.png"
+      },
+      {
+        title: "Download your shows to watch on the go.",
+        text: "Save your data and watch all your favorites offline.",
+        imgUrl:
+          "https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/mobile-0819.jpg"
+      },
+      {
+        title: "Watch everywhere.",
+        text:
+          "Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV without paying more.",
+        imgUrl:
+          "https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/device-pile.png"
+      }
+    ];
+    return (
+      <div className="content_container">
+        {content.map((cont, idx) => {
+          if (idx % 2 === 0) {
+            return (
+              <div className="content_container_rigth">
+                <div className="content_text">
+                  <h1>{cont.title}</h1>
+                  <h2>{cont.text}</h2>
+                </div>
+                <div className="content_image_container">
+                  <img src={cont.imgUrl} alt={cont.title} />
+                </div>
+              </div>
+            );
+          } else {
+            return (
+              <div className="content_container_left">
+                <div className="content_image_container">
+                  <img src={cont.imgUrl} alt={cont.title} />
+                </div>{" "}
+                <div className="content_text">
+                  <h1>{cont.title}</h1>
+                  <h2>{cont.text}</h2>
+                </div>
+              </div>
+            );
+          }
+        })}
+      </div>
+    );
+  }
+}
+
+export default Content;
