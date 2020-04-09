f4aa55954d4b8a891a16f94ee9f6593062fb76fe
Ingrid Amaral <guid_95@hotmail.com>
2020-04-09 11:10:15 +0200

    fix search bar

--- src/components/browse/Search.js
+++ src/components/browse/Search.js
@@ -4,7 +4,7 @@ import onClickOutside from "react-onclickoutside";
 
 class Search extends React.Component {
   state = {
-    openSearch: false
+    openSearch: false,
   };
 
   handleClickOutside = () => {
@@ -12,7 +12,7 @@ class Search extends React.Component {
   };
 
   handleClick = () => {
-    this.setState(state => ({ openSearch: !state.openSearch }));
+    this.setState((state) => ({ openSearch: !state.openSearch }));
   };
 
   render() {
@@ -20,7 +20,7 @@ class Search extends React.Component {
       height: "100rem",
       width: "1000rem",
       background: "#g6d5s5",
-      display: "block"
+      display: "block",
     };
 
     return (
@@ -31,22 +31,14 @@ class Search extends React.Component {
             ? {
                 border: ".05rem solid #fff",
                 background: "#141414",
-                width: "15rem"
+                width: "15rem",
               }
             : { width: "1.5rem" }
         }
       >
-        <svg
-          viewBox="0 0 20 20"
-        >
-          <path
-            fill="none"
-            d="M19.129,18.164l-4.518-4.52c1.152-1.373,1.852-3.143,1.852-5.077c0-4.361-3.535-7.896-7.896-7.896
-								c-4.361,0-7.896,3.535-7.896,7.896s3.535,7.896,7.896,7.896c1.934,0,3.705-0.698,5.078-1.853l4.52,4.519
-								c0.266,0.268,0.699,0.268,0.965,0C19.396,18.863,19.396,18.431,19.129,18.164z M8.567,15.028c-3.568,0-6.461-2.893-6.461-6.461
-								s2.893-6.461,6.461-6.461c3.568,0,6.46,2.893,6.46,6.461S12.135,15.028,8.567,15.028z"
-          ></path>
-        </svg>
+        <span className='search-icon' onClick={this.handleClick}>
+          <i className="fas fa-search"></i>
+        </span>
         <input
           style={
             this.state.openSearch
